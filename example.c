318005edf203ea97cf0c9e93efa32dddc9c67767
Arne Vansteenkiste <barnex@gmail.com>
2014-02-18 17:05:55 +0100

    example.c: cleanup astyle's mess

--- example.c
+++ example.c
@@ -9,12 +9,12 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    for(int i=1; i<argc; i++) {
+	for(int i=1; i<argc; i++) {
 		fprintf(stdout, "%s:\n", argv[i]);
-        ovf2_data d = ovf2_readfile(argv[i]);
-        ovf2_write(stdout, d);
+		ovf2_data d = ovf2_readfile(argv[i]);
+		ovf2_write(stdout, d);
 		fprintf(stdout, "\n");
-        ovf2_free(&d);
-    }
-    return 0;
+		ovf2_free(&d);
+	}
+	return 0;
 }
