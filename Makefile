3eaf6450fdec7ca528f306fa31e8e60d4b9335b5
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-05-21 11:32:27 +0200

    start unit tests

--- Makefile
+++ Makefile
@@ -2,9 +2,8 @@ libovf2:
 	gcc -c -std=c89 -Wall -Wextra -Werror libovf2.c
 	gcc -c -std=c99 -Wall -Wextra -Werror libovf2.c
 	g++ -c          -Wall -Wextra -Werror libovf2.c
+	gcc    -std=c99 -Wall -Wextra -Werror libovf2.c test.c     -o test
+	gcc    -std=c99 -Wall -Wextra -Werror libovf2.c ovf2tool.c -o ovf2tool
 
-ovf2tool: libovf2
-	gcc -std=c99 *.c -o ovf2tool
-
-test: 
-	./ovf2tool testdata_good/*.ovf
+test: libovf2
+	./test
---
+++ test.c
@@ -0,0 +1,57 @@
+#include "libovf2.h"
+
+
+int testScalarIndex(){
+	ovf2_data d = ovf2_readfile("testdata_good/scalar123.ovf");
+	if(d.err != NULL){
+		printf("%s", d.err);
+		return 1;
+	}
+	float val = ovf2_get(&d, 0, 1, 2, 3);
+	if(val != 4.0){
+		printf("want 4, got: %f\n", val);
+		return 1;
+	}
+	return 0;
+}
+
+
+int testVectorIndex(){
+	ovf2_data d = ovf2_readfile("testdata_good/vector123.ovf");
+	if(d.err != NULL){
+		printf("%s", d.err);
+		return 1;
+	}
+	float val = ovf2_get(&d, 0, 1, 2, 3);
+	if(val != 4.0){
+		printf("want 4, got: %f\n", val);
+		return 1;
+	}
+	val = ovf2_get(&d, 1, 1, 2, 3);
+	if(val != 5.0){
+		printf("want 5, got: %f\n", val);
+		return 1;
+	}
+	val = ovf2_get(&d, 3, 1, 2, 3);
+	if(val != 6.0){
+		printf("want 6, got: %f\n", val);
+		return 1;
+	}
+	return 0;
+}
+
+
+static int overall_status = 0;
+
+void check(char *name, int status){
+	printf("%s: %s\n", name, ((status==0) ? "OK": "FAIL"));
+	if (status != 0){
+		overall_status = 1;
+	}
+}
+
+int main(){
+	check("testScalarIndex", testScalarIndex());
+	check("testVectorIndex", testVectorIndex());
+	return overall_status;
+}
